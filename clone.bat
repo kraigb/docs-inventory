33cbab8c97b3ed85c82ab27d368ce8950a3400c7
Karl Erickson <karler@microsoft.com>
2019-03-08 15:03:08 -0800

    added batch files

---
+++ clone.bat
@@ -0,0 +1,9 @@
+pushd \repos
+git clone https://github.com/MicrosoftDocs/visualstudio-docs-pr
+git clone https://github.com/MicrosoftDocs/azure-docs-pr
+git clone https://github.com/Azure/azure-docs-sdk-java
+git clone https://github.com/MicrosoftDocs/azure-docs-sdk-python
+git clone https://github.com/MicrosoftDocs/azure-docs-sdk-node
+git clone https://dev.azure.com/mseng/TechnicalContent/_git/vsts-docs-pr
+git clone https://www.github.com/Microsoft/vscode-docs
+popd
---
+++ go.bat
@@ -0,0 +1,17 @@
+pushd ..\visualstudio-docs-pr
+git pull origin master
+cd ..\vscode-docs
+git pull origin master
+cd ..\vsts-docs-pr
+git pull origin master
+cd ..\azure-docs-sdk-java
+git pull origin master
+cd ..\azure-docs-sdk-python
+git pull origin master
+cd ..\azure-docs-sdk-node
+git pull origin master
+cd ..\azure-docs-pr
+git pull origin master
+popd
+python take-inventory.py
+move *.csv \InventoryData
